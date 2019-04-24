073b26bd35fe275d2b0b8df1090d747e1d117d52
Anjan Kumar <itsanjan2015@gmail.com>
2019-04-24 22:25:34 +0530

    added hacker rank problems

---
+++ hackerrank/mathematics/test_P01_find_the_point.py
@@ -0,0 +1,7 @@
+import pytest
+import E:\Repo\generic-python\hackerrank\mathematics\test_P01_find_the_point.py
+
+def test_findPoint():
+	x=5
+	y=6
+	assert P01_find_the_point.findPoint(0,0,1,1) == (2,2)
